3d53445f9d031e7fd485192bd52edc964543cb57
monish0001 <mohdmonishksg@gmail.com>
2024-07-09 22:33:49 +0530

    create table and database added

---
+++ Create_cmd.sql
@@ -0,0 +1,18 @@
+--CREATE DATABASE testDB
+
+-- CREATE TABLE Customer(
+-- 	id_ INT PRIMARY KEY,
+-- 	customer_name VARCHAR(100),
+-- 	age INT,
+-- 	city VARCHAR(50),
+-- 	salary numeric
+-- );
+
+-- DROP TABLE Customer;
+
+-- INSERT INTO Customer(id_,customer_name,age,city,salary)
+-- VALUES (1,'sam',25,'amroha',43235),
+-- (2,'ram',12,'delhi',67898),
+-- (3,'karan',25,'agra',9807);
+
+-- SELECT * FROM Customer;
